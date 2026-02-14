244ec0fd1a15e0869a59af9c19ab18098014018f
Decent-Nazim <muhammadnazimrind@gmail.com>
2026-02-14 14:21:37 +0500

    NextGen-XMD

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SRhhUbLA#-y4K2TRcsDPKzqyDwHMFabOQLKgNvcv6okmqTa6UG_I",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/nazimhihoo/NextGen-XMD/blob/main/images/NextGen-MD.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hay👋 NextGen-MD Is Alive Now😍*",
 BOT_OWNER: '923229168159',  // Replace with the owner's phone number
